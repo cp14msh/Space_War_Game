c283a3816c53738d3293ecd8a7451601778c09f6
Hasan Shahroodi <msh37037@gmail.com>
2025-12-22 20:18:26 +0330

    add enemy to game

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <windows.h>
 #include <chrono>
 #include <thread>
+#include <time.h>
 
 using namespace std;
 using namespace std::chrono;
@@ -20,6 +21,14 @@ struct Bullet
 {
     int x, y;
     bool active;
+};
+
+struct Enemy
+{
+    int x, y;
+    bool active;
+
+    steady_clock::time_point lastMoveTime;
 };
 
 class Player
@@ -62,6 +71,7 @@ int main()
     // --- Initialization ---
     Player player(WIDTH / 2 - 2, HEIGHT - 5);
     vector<Bullet> bullets;
+    vector<Enemy> enemies;
     bool gameOver = false;
 
     // Console Setup: Hide cursor to avoid flickering artifacts
@@ -74,6 +84,8 @@ int main()
     // Timing variables
     auto lastShotTime = steady_clock::now();
     auto lastFrameTime = steady_clock::now();
+    auto lastEnemySpawnTime = steady_clock::now();
+    srand(time(0));
 
     // --- Main Game Loop ---
     while (!gameOver)
@@ -116,6 +128,49 @@ int main()
                 i++;
         }
 
+        // Enemy spawn limiter: 1000ms
+        auto now = steady_clock::now();
+        if (duration_cast<milliseconds>(now - lastEnemySpawnTime).count() > 1000)
+        {
+            Enemy e;
+            e.y = 1;
+
+            e.x = rand() % (WIDTH - 2) + 1;
+            e.active = true;
+            e.lastMoveTime = steady_clock::now();
+
+            enemies.push_back(e);
+            lastEnemySpawnTime = now;
+        }
+
+        // Move enemies down
+        for (int i = 0; i < enemies.size(); i++)
+        {
+            auto now = steady_clock::now();
+
+            if (duration_cast<milliseconds>(now - enemies[i].lastMoveTime).count() > 150)
+            {
+                enemies[i].y++;
+                enemies[i].lastMoveTime = now;
+            }
+
+            if (enemies[i].y > HEIGHT)
+                enemies[i].active = false;
+        }
+
+        // Remove inactive enemies
+        for (int i = 0; i < enemies.size();)
+        {
+            if (!enemies[i].active)
+            {
+                enemies.erase(enemies.begin() + i);
+            }
+            else
+            {
+                i++;
+            }
+        }
+
         // 3. Rendering
         // Reset cursor to top-left (0,0) to overwrite previous frame
         SetConsoleCursorPosition(hOut, {0, 0});
@@ -136,6 +191,16 @@ int main()
                         break;
                     }
                 }
+                // Check if current (x,y) contains a enemy
+                bool isEnemy = false;
+                for (auto &e : enemies)
+                {
+                    if (e.x == x && e.y == y)
+                    {
+                        isEnemy = true;
+                        break;
+                    }
+                }
 
                 // Check collision/overlap with player coordinates
                 bool insideX = (x >= player.x && x < player.x + player.width);
@@ -145,6 +210,8 @@ int main()
                     buffer += player.shape[y - player.y][x - player.x]; // Draw ship part
                 else if (isBullet)
                     buffer += "|";
+                else if (isEnemy)
+                    buffer += ">o<";
                 else
                     buffer += " ";
             }
@@ -156,8 +223,8 @@ int main()
 
         // 4. Frame Pacing (FPS Cap)
         // Ensures consistent speed across different hardware
-        auto now = steady_clock::now();
-        auto elapsed = duration_cast<milliseconds>(now - lastFrameTime);
+        auto endnow = steady_clock::now();
+        auto elapsed = duration_cast<milliseconds>(endnow - lastFrameTime);
 
         if (elapsed < milliseconds(FRAME_DURATION_MS))
         {
