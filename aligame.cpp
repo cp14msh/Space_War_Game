e337e6e312a6e99974ee26e3e9f11a7f53720030
Ali Gaeini <aligaeeni1386@gmail.com>
2025-12-29 19:36:23 +0330

    Initial setup and definition of main structs

---
+++ aligame.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct Bullet {
+    float x, y;
+    int height, width, speed;
+};
+struct Player {
+    float x, y;
+    int height, width, speed;
+};
+struct Enemy {
+    float x, y;
+    int height, width, speed;
+};
