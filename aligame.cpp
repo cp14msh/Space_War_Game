27b091166390d469adc15e6407e1bc96c77956a7
Ali Gaeini <aligaeeni1386@gmail.com>
2025-12-29 19:44:56 +0330

    Determine the game board and player Movement

--- aligame.cpp
+++ aligame.cpp
@@ -13,3 +13,19 @@ struct Enemy {
     float x, y;
     int height, width, speed;
 };
+int main() {
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
+    Player player;
+        player.x = SCREEN_WIDTH / 2;
+        player.y = SCREEN_HEIGHT - 50;
+        player.height = 20;
+        player.width = 40;
+        player.speed = 5;
+    if (player.x < 0) {// محدودیت حرکت سفینه
+        player.x = 0;
+    }
+    if (player.x + player.width > SCREEN_WIDTH) {
+        player.x = SCREEN_WIDTH - player.width;
+    }
+}
